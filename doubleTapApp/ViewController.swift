2532f61393f1cf4c0859be30cafe8a98c8f71534
supreeth1816 <supreet.venkata@gmail.com>
2020-08-25 15:51:49 +0530

    Added doubleTapGesture

--- doubleTapApp/ViewController.swift
+++ doubleTapApp/ViewController.swift
@@ -9,12 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView(frame: CGRect(x:0, y:0, width:400, height: 400))
+        imageView.contentMode = .scaleAspectFit
+        imageView.isUserInteractionEnabled = true
+        imageView.image = UIImage(named: "image")
+        
+        return imageView
+    }()
+    
+    /*
+    private let imageTitle: UILabel = {
+        
+        let imageTitle = UILabel()
+        imageTitle.text = "SOS Poster"
+        
+        return imageTitle
+        
+    }()
 
+ */
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+       
+        
+        view.addSubview(imageView)
+        imageView.center = view.center
         // Do any additional setup after loading the view.
+        
+        let doubleTapGesture = UITapGestureRecognizer(target: self,
+                                                      action: #selector(didDoubleTap(_:)) )
+        
+        doubleTapGesture.numberOfTapsRequired = 2
+        imageView.addGestureRecognizer(doubleTapGesture)
+    }
+    
+   @objc private func didDoubleTap(_ gesture: UITapGestureRecognizer){
+    print("double tapped")
+        
     }
 
 
+
+
 }
+
 
